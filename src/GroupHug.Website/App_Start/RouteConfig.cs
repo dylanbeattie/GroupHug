e69f474ec268c7a995cb3ad41088b6549ea3094d
Dylan Beattie <dylan.beattie@spotlight.com>
2014-07-09 18:54:58 +0100

    Default controller, blank view, enabled Windows authentication

--- src/GroupHug.Website/App_Start/RouteConfig.cs
+++ src/GroupHug.Website/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace GroupHug.Website {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "People", id = UrlParameter.Optional }
             );
         }
     }
---
+++ src/GroupHug.Website/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupHug.Website.Controllers {
+    public class HomeController : Controller {
+        //
+        // GET: /Home/
+
+        public ActionResult People() {
+            return View();
+        }
+
+    }
+}
