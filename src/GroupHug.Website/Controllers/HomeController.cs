c843fdc4532e801eb741e1314be5772d9997eace
Dylan Beattie <dylan.beattie@spotlight.com>
2014-07-09 19:03:51 +0100

    Added basic views, ViewStart and Layout.cshtml

--- src/GroupHug.Website/Controllers/HomeController.cs
+++ src/GroupHug.Website/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ using System.Web.Mvc;
 
 namespace GroupHug.Website.Controllers {
     public class HomeController : Controller {
-        //
-        // GET: /Home/
 
+        public ActionResult Index() {
+            return View();
+        }
         public ActionResult People() {
             return View();
         }
-
     }
 }
